e5d7562dfcb1a71ae38cf17cc28498fbc69fac7e
celinelee522 <celinelee522@gmail.com>
2017-11-16 12:57:25 +0900

    client.c

--- client.c
+++ client.c
@@ -16,25 +16,23 @@ int main(int argc, char *argv[]){
 
 	pthread_t thread_t[5];
 	struct sockaddr_in server_addr;
-	struct sockaddr_in client_addr;
-	int server_fd, client_fd[5];	
+	int client_fd[5];	
 	int port[5];
 
 	printf("Type Server Ports(format : <Port1> <Port2> <Port3> <Port4> <Port5>");
 	scanf("%d %d %d %d %d", &port[1], &port[2], &port[3], &port[4], &port[5]);
-
-	if ( -1 == (client_fd = socket(AF_INET, SOCK_STREAM, 0))){
-		printf("creation failed\n");
-		exit(0);
-	}
 
 	for (int i = 0; i < 5; i++){
 		memset(&server_addr, 0, sizeof(server_addr));
 		server_addr.sin_family = AF_INET;
 		server_addr.sin_port = htons(port[i]);
 		server_addr.sin_addr.s_addr = inet_addr();
-	
-		if ( -1 == connect(client_fd[i], (struct sockaddr*)&server_addr), sizeof(server_addr)){
+		
+		if ( (client_fd[i] = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
+			printf("creation failed\n");
+			exit(0);
+		}
+		if ( connect(client_fd[i], (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1 ){
 			printf("connection failed\n");
 		}
 		if (pthread_create(&thread_t[i], NULL, connection_handler, (void*)&client_fd[i]) < 0){
@@ -51,7 +49,11 @@ int *connection_handler(void *client_fd){
 
 	read(client_fd, buffer, BUF_LEN);
 
-	FILE *file = fopen(client_fd.sin_port, "a");
+	char filename[2];
+	filename[1] = client_fd.sin_port + '0';
+	filename[2] = ".txt"
+
+	FILE *file = fopen( *filename , "a");
 
 	struct tm *tm_struct = localtime(time(NULL));
 
