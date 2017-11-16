4c44a0ae62c17be9df3d1699ff90605cb6e41e7f
celinelee522 <celinelee522@gmail.com>
2017-11-16 16:10:00 +0900

    client

--- client.c
+++ client.c
@@ -6,13 +6,14 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <pthread.h>
+#include <unistd.h>
 
 #define BUF_LEN 128
 char buffer[BUF_LEN];
 
 int *connection_handler(void *);
 
-int main(int argc, char *argv[]){
+int main(){
 
 	pthread_t thread_t[5];
 	struct sockaddr_in server_addr;
@@ -20,13 +21,14 @@ int main(int argc, char *argv[]){
 	int port[5];
 
 	printf("Type Server Ports(format : <Port1> <Port2> <Port3> <Port4> <Port5>");
-	scanf("%d %d %d %d %d", &port[1], &port[2], &port[3], &port[4], &port[5]);
+	scanf("%d %d %d %d %d", &port[0], &port[1], &port[2], &port[3], &port[4]);
 
 	for (int i = 0; i < 5; i++){
+		printf("iteration : %dth\n", i);
 		memset(&server_addr, 0, sizeof(server_addr));
 		server_addr.sin_family = AF_INET;
 		server_addr.sin_port = htons(port[i]);
-		server_addr.sin_addr.s_addr = inet_addr();
+		server_addr.sin_addr.s_addr = inet_addr("192.168.99.100");
 		
 		if ( (client_fd[i] = socket(AF_INET, SOCK_STREAM, 0)) == -1 ){
 			printf("creation failed\n");
@@ -40,18 +42,19 @@ int main(int argc, char *argv[]){
 			exit(0);
 		}
 	
-		delay(5000); // wait for 5 seconds
+		usleep(5000); // wait for 5 seconds
 	}
 	return 0;
 }
 
 int *connection_handler(void *client_fd){
 
+	printf("connection handler called\n");
 	read(client_fd, buffer, BUF_LEN);
 
 	char filename[2];
-	filename[1] = client_fd.sin_port + '0';
-	filename[2] = ".txt"
+	filename[1] = '0';
+	filename[2] = ".txt";
 
 	FILE *file = fopen( *filename , "a");
 
